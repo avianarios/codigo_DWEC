b90d7f6b97710c933f1c52095322d9356cc857ab
avianarios <alejandro@micorreo.com>
2023-09-24 21:07:02 +0200

    añadidos conversiones y operadores

---
+++ fuente/js/conversiones.js
@@ -0,0 +1,24 @@
+//string conversion
+//Mal por llamar cadena a un booleano
+let cadena = true;
+console.log (typeof cadena); // boolean
+
+cadena = String(cadena) // now value is a string "true"
+console.log (typeof cadena); // string
+
+//Integer conversion
+console.log ( "4"/"2" );
+let cadena2="123";
+
+console.log (typeof(cadena2));
+let num=Number(cadena2);
+console.log (typeof(num));
+
+//Boolean conversion
+//Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
+console.log (Boolean(1));
+console.log (Boolean(NaN));
+console.log (Boolean(undefined));
+console.log (Boolean(0));
+console.log (Boolean(11123123));
+console.log (Boolean(""));
--- fuente/js/variables.js
+++ fuente/js/variables.js
@@ -2,22 +2,32 @@
 "use strict";
 
 //ejemplo del uso de variables, constantes y tipos
-//las variables se deben definir con let. var es anticuado
-let numero1=5, numero2=10;
-let cadena="hola don pepito";
-let asignaturas=["Desarrollo web en entorno cliente", "Diseño de interfaces web"];
-let persona= {nombre:"procopio", apellido: "Maximus"};
+//las variables se deben definir con let. var está anticuado
+//Ejemplos de mala sintaxis. Funciona, pero no está recomendado
+let numero1=5, numero2=10, cadena="hola don pepito";    //no recomendable
+cadena="5";
+const NUMERO=7, CADENA="6";
 
-console.log ( numero + cadena );
+//Buena sintaxis
+const ROJO="#FF0000";
+let numero3=1,
+    numero4=2,
+    numero5=3;
+let numero6=4;
+let numero7=5;
+let asignaturas=["Desarrollo web en entorno cliente", "Diseño de interfaces web"];  //matriz
+let persona= {nombre:"procopio", apellido: "Maximus"};  //objeto
+
+//salida por pantalla
+console.log ( numero1 + cadena );
 console.log ( `La suma de ${numero1} y ${numero2} da ${numero1+numero2}` );
 console.log ( numero1+numero2 );
 console.log( asignaturas[1] );
 console.log( persona.nombre )
+console.log ( typeof(numero1), typeof(cadena), typeof(asignaturas), typeof(persona), typeof( constante) );
+console.log ( 4/"2" );
 
-cadena="5";
-const NUMERO=7, CADENA="6";
-
-
+//Mal uso de la constante
 const NOMBRE = prompt("Dígame su nombre");
 console.log (`Encantado de saludarle, ${NOMBRE}, tengo entendido que tiene ${prompt ("dígame su edad")} años`);
 alert (`Buenos días ${NOMBRE}`);
@@ -29,5 +39,5 @@ console.log (`El resultado de hacer la operación numero+NUMERO es ${numero+NUME
 /*Las variables en javascript son débilmente tipado, es decir, no declaro su tipo y éste puede cambiar a lo largo de la vida de la variable
 Es recomendable que los nombres de las variables sean lo más descriptivos posible. El siguiente caso es una mala práctica*/
 
-cadena=5 //¡la variable llamada "cadena" es de tipo entero! ¡¡MAL!!
+cadena=5 //Estoy cambiándole el tipo a entero una variable llamada cadena.¡¡MAL!!
 console.log ("El resultado de hacer la operación numero+cadena es", numero+cadena, `que es de tipo ${typeof(numero+cadena)}` );
