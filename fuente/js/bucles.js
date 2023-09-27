8f544529289a424954d787fd45ac7cefdb9f3947
avianarios <alejandro@micorreo.com>
2023-09-27 17:08:04 +0200

    nuevo operador y bucles

---
+++ fuente/js/bucles.js
@@ -0,0 +1,12 @@
+//while
+
+let i = 0;
+while (i < 3) {
+  console.log( i++ ); //it outputs i value and then increment it
+}
+
+let j = 0;
+while (j < 3) {
+  console.log( ++j ); //it increments j value and then shows it
+}
+
