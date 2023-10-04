7f8f97cb1cd29c1a07fb1b42166636c14ed28da3
Alejandro <prueba@hotmail.com>
2023-10-04 14:12:01 +0200

    añadido tipo numero

---
+++ fuente/js/numeros.js
@@ -0,0 +1,15 @@
+"use sctrict";
+
+let billon=1e12;
+let millon=1e6;
+let microsegundo=1e-6;
+
+let ROJO=0xff0000;
+
+//Devuelve verdadero si el primer número es mayor al segundo y falso si no
+function comparaNumero(a,b){
+    return (a>b);
+}
+
+comparaNumero(billon,millon);
+console.log(ROJO);
