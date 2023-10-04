9b6ecfaa7825df2129ca5d9dcedd37d50f8cde23
Alejandro <prueba@hotmail.com>
2023-10-04 14:39:38 +0200

    cambios en numeros

--- fuente/js/numeros.js
+++ fuente/js/numeros.js
@@ -3,6 +3,8 @@
 let billon=1e12;
 let millon=1e6;
 let microsegundo=1e-6;
+let num1=255;
+
 
 let ROJO=0xff0000;
 
@@ -11,5 +13,7 @@ function comparaNumero(a,b){
     return (a>b);
 }
 
+
 comparaNumero(billon,millon);
 console.log(ROJO);
+console.log (num1.toString(8), num1.toString(16), num1.toString(2));
