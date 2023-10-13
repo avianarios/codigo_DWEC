79a0768d244cc352ca30a1bb9250134d25e7f670
avianarios <alejandro@micorreo.com>
2023-10-13 16:43:12 +0200

    añadido tipo objeto

---
+++ fuente/js/tipo_dato_objeto.js
@@ -0,0 +1,21 @@
+let usuario={
+    id:"1", nombre:"pepe", edad:30
+};
+
+console.log(usuario.nombre);
+usuario.esAdmin=false;
+console.log(usuario);
+delete(usuario.edad);
+console.log(usuario);
+
+//brackets allows to calculate in real-time the key 
+let llave=prompt("¿Qué quieres saber del usuario?");  //needs to be a valid key name
+console.log(usuario[llave]);    //llave=edad or nombre...
+
+//another example of brackets
+let llave2=prompt("¿Qué elemento quieres?");
+let cantidad={
+    [llave2]: 5
+}
+console.log (cantidad);
+console.log (cantidad[llave2]);
