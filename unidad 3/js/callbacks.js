90f122d13fab76cbfb0c9cb6d3b10935dab2b24c
avianarios <alejandro@micorreo.com>
2023-11-02 13:53:43 +0100

    creación de clases y funciones de retorno

--- unidad 3/js/callbacks.js
+++ unidad 3/js/callbacks.js
@@ -1,35 +1,4 @@
-///////////CALLBACK FUNCTIONS (funciones de retorno)//////////////
-function ask(question, yes, no) {
-    if (confirm(question)) yes()  //confirm shows up a window asking accept or cancel
-    else no();
-  }
-  
-  //showOk and showCancel are called callback functions
-  function showOk() {
-    console.log( "You agreed." );
-  }
-  
-  function showCancel() {
-    console.log( "You canceled the execution." );
-  }
-  
-  // usage: functions showOk, showCancel are passed as arguments to ask
-  ask("Do you agree?", showOk, showCancel);
-  
-  
-  
-  //Another way to express the last code (not recommended tough)
-  function pregunta(question, yes, no) {
-    if (confirm(question)) yes()  //confirm shows up a window asking accept or cancel
-    else no();
-  }
-  
-  pregunta(
-    "Agree?",
-    function() {console.log("estás de acuerdo");},
-    function () { console.log ("no estás de acuerdo");}
-  );
-  
+
   
   //yet another callback example
   function solicitudServidor(consulta, callback){
