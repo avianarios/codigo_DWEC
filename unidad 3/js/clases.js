3d1be97e17391f0ce8b845ab2f2a1f4b4a1ecacf
avianarios <alejandro@micorreo.com>
2023-11-03 14:52:15 +0100

    clases

--- unidad 3/js/clases.js
+++ unidad 3/js/clases.js
@@ -1,6 +1,7 @@
-//New allows us to create objects of the same type, but class is more advanced
+//"new" allows to create objects of the same type, but class is more advanced
 class User {
 
+    //automatically called by new
     constructor(name) {
       this.name = name;
     }
@@ -15,3 +16,33 @@ class User {
   
 let user = new User("Apolinar");
 user.sayHi();
+
+/*
+class methods are non-enumerable. you can not iterate through them with for..in
+classes always are in strict mode
+*/
+
+//////////class as an expression//////////
+let perro=class{
+  ladra(){
+    console.log("guau, guau");
+  }
+};
+
+let a=new perro();
+a.ladra();
+new perro().ladra();
+
+
+//you can provide a name, just like functions
+//the name is only visible inside the class
+let gatito=class gato{
+  maulla(){
+    console.log("miau, miau");
+    console.log(gato);  //a class is a function!!
+  }
+}
+//let c=new gato();   //error
+new gatito().maulla();
+let b=new gatito();
+b.maulla();
