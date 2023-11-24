9f9a73ff6716f5717617f25063193bb6d19e3194
avianarios <alejandro@micorreo.com>
2023-11-24 17:09:45 +0100

    objeto location

--- unidad 3/js/a.js
+++ unidad 3/js/a.js
@@ -1,1 +1,13 @@
-console.log (window.location);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    //document.getElementById("demo").innerHTML =
+    console.log (    "Geolocation is not supported by this browser.");
+  }
+  
+  function showPosition(position) {
+  /*  document.getElementById("demo").innerHTML =
+    "Latitude: " + position.coords.latitude + "<br>" +
+    "Longitude: " + position.coords.longitude;*/
+    console.log (`latitud: ${position.coords.latitude}, longitud: ${position.coords.longitude}`);
+  }
