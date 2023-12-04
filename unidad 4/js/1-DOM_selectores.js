5d02fffef35cbe7350dce5018730a93c882106e4
avianarios <alejandro@micorreo.com>
2023-12-04 14:24:56 +0100

    nuevos atributos y propiedades

--- unidad 4/js/1-DOM_selectores.js
+++ unidad 4/js/1-DOM_selectores.js
@@ -1,9 +1,8 @@
 /*main sources:
 https://www.digitalocean.com/community/tutorials/how-to-access-elements-in-the-dom
+https://es.javascript.info/searching-elements-dom
 https://www.javascripttutorial.net/javascript-dom/
 https://www.w3schools.com/
-
-
 */
 
 /////////////////////
@@ -30,12 +29,18 @@ console.log (aux3[0], aux3.length);
 let aux4=document.getElementsByName("nombre");
 console.log (aux4[0],aux4.length);
 
+//all getElementsBy* returns a live collection, meaning that if an element is created after getElementsBy* was called, this collection is updated
+
 ////querySelector////
 //returns the very first element within the document that matches the given selector. It only returns the element that matches with one of the specified CSS selectors, or a group of selectors.
 let seccion=document.querySelector("section");
 console.log(seccion);
 
 ////querySelectorAll////
-//returns a static NodeList of elements that matches with one or a group of selectors. If no element matches, an empty NodeList is returned.
+//returns a static NodeList of elements that matches with one or a group of CSS selectors. If no element matches, an empty NodeList is returned.
+//if an element is created after querySelectorAll was called, the static nodelist is NOT updated
 let secciones=document.querySelectorAll("section");
-console.log (secciones);
+console.log (secciones, secciones[0].textContent);
+
+let elements=document.querySelectorAll('ul > li:last-child');   //CSS selectors can be used
+console.log (elements.length, elements[0].innerHTML);   //although there is only 1 element, it is still an array
--- unidad 4/js/3-DOM_CSS.js
+++ unidad 4/js/3-DOM_CSS.js
@@ -39,28 +39,7 @@ const miEstilo= `
 `;
 elemento.style.cssText=miEstilo;
 
-///////////////////////////////////
-////manipulating tag attributes////
-///////////////////////////////////
-////hasAttribute, getAttribute, setAttribute, removeAttribute////
-//1- Checks if a tag has an attribute or not
-//2- Gets attribute value
-//3- Sets attribute value
-//4- Removes attribute from tag
-//example 1: changing img src
-let imagen=document.querySelector("img");
-if (imagen.hasAttribute('src')){
-    console.log ("La imagen actual es "+imagen.getAttribute('src'));
-    imagen.setAttribute('src', 'https://picsum.photos/300');
-}
 
-//example 2: removing id from paragraphs
-let parrafos=Array.from (document.getElementsByTagName("p"));
-parrafos.forEach(element => {
-        if (element.hasAttribute('id')){
-            element.removeAttribute('id');
-        }
-});
 
 ////////////////////////////////////////////
 ////add, remove, replace, toggle classes////
