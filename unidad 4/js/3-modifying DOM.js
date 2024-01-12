029de6c24a0c1f445e38f2f572233a8c924a5a21
avianarios <alejandro@micorreo.com>
2024-01-12 14:52:09 +0100

    añadido código para crear dom temporal

--- unidad 4/js/3-modifying DOM.js
+++ unidad 4/js/3-modifying DOM.js
@@ -3,18 +3,49 @@
 //////////////////////////////
 
 
-
-////createElement, createTextNode, appendChild////
+////createElement, createTextNode, createComment, appendChild////
 //1: creates a node
 //2: creates a text element
-//3: makes a node to be child of another one
-//example 1: creates a new section with a p section and a text on it
-const seccion=document.createElement("section");
-const nodo=document.createElement("p");
-const texto=document.createTextNode("hola don pepito");
+//3: creates a comment
+//4: makes a node to be child of another one
+//example 1: creates a new section with a comment and a paragraph on it
+let seccion=document.createElement("section");
+let comentario=document.createComment("esta sección no tiene utilidad alguna");
+let nodo=document.createElement("p");
+let texto=document.createTextNode("hola don pepito");
+seccion.appendChild(comentario);
 seccion.appendChild(nodo);
 nodo.appendChild(texto);
 document.body.appendChild(seccion);
+
+////createDocumentFragment////
+//creates a temporary DOM, disconnected from the real one, avoiding having to make recurrent changes into the real DOM and boosting performance
+const temporal=document.createDocumentFragment();
+temporal.appendChild(document.createComment("esto es una sección temporal"));
+temporal.appendChild(seccion);
+let aux;
+for (let i=0; i<5; i++){
+    parrafo=document.createElement("p");
+    texto=document.createTextNode(`párrafo número ${i}`);
+    parrafo.appendChild(texto);
+    seccion.appendChild(parrafo);
+}
+temporal.appendChild(seccion);
+console.log (temporal);
+setTimeout(()=>{
+	document.body.appendChild(temporal);  
+},3000);
+
+////cloneNode(arg), isConnected////
+//if arg=true, it also clones children
+const seccion_clonada=seccion.cloneNode(false);
+console.log (seccion_clonada.isConnected);  //false
+document.body.appendChild(seccion_clonada);
+console.log (seccion_clonada.isConnected);  //true
+
+////removeChild////
+document.body.querySelector("p")
+zzzzzzz
 
 ////replaceWith, parentNode, insertBefore////
 //1: Replaces a node with another one
