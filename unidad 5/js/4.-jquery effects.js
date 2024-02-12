039f241b2a9765e23f7a978849bd9a58148cb304
avianarios <avr.web@protonmail.com>
2024-02-12 15:08:21 +0100

    efectos en jquery y corrección de errores

--- unidad 5/js/4.-jquery effects.js
+++ unidad 5/js/4.-jquery effects.js
@@ -7,119 +7,214 @@
 //A ms counter can be set and a callback function as well
 
 $(()=>{
-    $("#show-and-hide > p:not(#texto-show-hide)").css("background-color", "lightyellow");
-    $("#show-and-hide > p:not(#texto-show-hide)").css("width", "100%");
-    $("#show-and-hide > p:not(#texto-show-hide)").css("height", "50px");
+    /*$(".parrafos-show-hide-toggle > p:not(#texto-show-hide-toggle)").css("background-color", "lightyellow");
+    $(".parrafos-show-hide-toggle > p:not(#texto-show-hide-toggle)").css("width", "100%");
+    $(".parrafos-show-hide-toggle > p:not(#texto-show-hide-toggle)").css("height", "50px");
+    $(".parrafos-toggle > p:not(#texto-toggle)").css("background-color", "lightyellow");
+    $(".parrafos-toggle > p:not(#texto-toggle)").css("width", "100%");
+    $(".parrafos-toggle > p:not(#texto-toggle)").css("height", "50px");*/
 
-//fast=200ms and slow=50ms
-    $("#hide-btn").click(function(){
-        $("#show-and-hide > p.very-slow").hide(2000, ()=>{
-            $("p#texto-show-hide").show();
-            $("p#texto-show-hide").text("paragraphs hided")
+    $(".parrafos ").css("width", "100%");
+    $(".parrafos > p").css("width", "100%");
+    $(".parrafos > p").css("background-color", "lightyellow");
+    $(".parrafos > p").css("height", "50px");
+
+    //hide
+    //fast=200ms and slow=50ms
+    $("#hide-btn").click(()=>{
+        $("#show-hide-toggle > .parrafos > p.very-slow").hide(2000, ()=>{
+            $("p#texto-show-hide-toggle").show();
+            $("p#texto-show-hide-toggle").text("paragraphs hided")
         });
-        $("#show-and-hide > p.slow").hide("slow");
-        $("#show-and-hide > p.normal").hide();
-        $("#show-and-hide > p.fast").hide("fast");
-        $("#show-and-hide > p.very-fast").hide(50);
+        $("#show-hide-toggle > .parrafos > p.slow").hide("slow");
+        $("#show-hide-toggle > .parrafos > p.normal").hide();
+        $("#show-hide-toggle > .parrafos > p.fast").hide("fast");
+        $("#show-hide-toggle > .parrafos > p.very-fast").hide(50);
     });
     
-    // Show hidden paragraphs
-    $("#show-btn").click(function(){
-        $("#show-and-hide > p.very-slow").show(2000)
-        $("#show-and-hide > p.slow").show("slow");
-        $("#show-and-hide > p.normal").show();
-        $("#show-and-hide > p.fast").show("fast");
-        $("#show-and-hide > p.very-fast").show(50, ()=>{
-            $("p#texto-show-hide").hide();
+    // Show
+    $("#show-btn").click(()=>{
+        $("#show-hide-toggle > .parrafos > p.very-slow").show(2000)
+        $("#show-hide-toggle > .parrafos> p.slow").show("slow");
+        $("#show-hide-toggle > .parrafos > p.normal").show();
+        $("#show-hide-toggle > .parrafos > p.fast").show("fast");
+        $("#show-hide-toggle > .parrafos > p.very-fast").show(50, ()=>{
+            $("p#texto-show-hide-toggle").hide();
         });
     });
   
+    //toggle
+    $("#toggle-btn").click(()=>{
+        $("#show-hide-toggle > .parrafos > p.very-slow").toggle(2000, ()=>{
+            $("p#texto-show-hide-toggle").toggle();
+            $("p#texto-show-hide-toggle").text("paragraphs hided")
+        });
+        $("#show-hide-toggle > .parrafos > p.slow").toggle("slow");
+        $("#show-hide-toggle > .parrafos > p.normal").toggle();
+        $("#show-hide-toggle > .parrafos > p.fast").toggle("fast");
+        $("#show-hide-toggle > .parrafos > p.very-fast").toggle(50);
+    })
+
+    //fadeIn
+    $("#fadeIn-btn").click(()=>{
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-slow").fadeIn(2000, ()=>{
+            $("p#texto-fadeIn-fadeOut-fadeToggle").fadeOut();
+        });
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.slow").fadeIn("slow");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.normal").fadeIn();
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.fast").fadeIn("fast");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-fast").fadeIn(50);
+    });
+
+    //fadeOut
+    $("#fadeOut-btn").click(()=>{
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-slow").fadeOut(2000, ()=>{
+            $("p#texto-fadeIn-fadeOut-fadeToggle").fadeIn();
+            $("p#texto-fadeIn-fadeOut-fadeToggle").text("paragraphs hided")
+        });
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.slow").fadeOut("slow");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.normal").fadeOut();
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.fast").fadeOut("fast");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-fast").fadeOut(50);
+    });
+
+    //fadeToggle
+    $("#fadeToggle-btn").click(()=>{
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-slow").fadeToggle(2000, ()=>{
+            $("p#texto-fadeIn-fadeOut-fadeToggle").fadeToggle();
+            $("p#texto-fadeIn-fadeOut-fadeToggle").text("paragraphs hided")
+        });
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.slow").fadeToggle("slow");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.normal").fadeToggle();
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.fast").fadeToggle("fast");
+        $("#fadeIn-fadeOut-fadeToggle > .parrafos > p.very-fast").fadeToggle(50);
+    });
+
+    //fadeTo
+    $("#fadeTo-btn").click(()=>{
+        let opacidad=parseFloat($('[name="opacidad"]').val());
+
+        $("#fadeTo > .parrafos > p.very-slow").fadeTo(2000, opacidad, ()=>{
+            $("p#texto-fadeTo").fadeToggle();
+            $("p#texto-fadeTo").text("paragraphs hided")
+        });
+        $("#fadeTo > .parrafos > p.slow").fadeTo("slow", opacidad);
+        $("#fadeTo > .parrafos > p.normal").fadeTo(125, opacidad);
+        $("#fadeTo > .parrafos > p.fast").fadeTo("fast",opacidad);
+        $("#fadeTo > .parrafos > p.very-fast").fadeTo(50,opacidad);
+    });
+
+    //slideUp
+    $("#slideUp-btn").click(()=>{
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-slow").slideUp(2000, ()=>{
+            $("p#texto-slideUp-slideDown-slideToggle").fadeToggle();
+            $("p#texto-slideUp-slideDown-slideToggle").text("paragraphs hided")
+        });
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.slow").slideUp("slow");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.normal").slideUp();
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.fast").slideUp("fast");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-fast").slideUp(50);
+    });
+
+    //slideDown
+    $("#slideDown-btn").click(()=>{
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-slow").slideDown(2000, ()=>{
+            $("p#texto-slideUp-slideDown-slideToggle").fadeToggle();
+        });
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.slow").slideDown("slow");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.normal").slideDown();
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.fast").slideDown("fast");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-fast").slideDown(50);
+    });
+
+    //slideToggle
+    $("#slideToggle-btn").click(()=>{
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-slow").slideToggle(2000, ()=>{
+            $("p#texto-slideUp-slideDown-slideToggle").slideToggle();
+        });
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.slow").slideToggle("slow");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.normal").slideToggle();
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.fast").slideToggle("fast");
+        $("#slideUp-slideDown-slideToggle > .parrafos > p.very-fast").slideToggle(50);
+    });       
+
 });
 
-//toggle
 
+//////////////////
+////animations////
+//////////////////
+//elements must have relative, fixed, or absolute position to manipulate or animate its position
+//optional parameters: delay and callback function
 
 
 $(()=>{
-    //REMEMBER. "this" does not work with arrow functions
-    $("#click").click(function(){
-        $(this).slideUp();
+    let delay=5000;
+    $("#animation-move-right").click(function(){
+        $("#animation-move-right").animate({
+/*            left: function(){
+                return $(this).parent().width();
+            }*/
+            left: 400
+        },delay);   //delay is optional
     });
 
-    $("#double-click").dblclick(function(){
-        $(this).slideUp();
+    $("#stop-move-right").click(()=>{
+        $("#animation-move-right").stop();
+    })
+
+
+    $("#animation-multiple").click(function(){
+        $("#animation-multiple").animate({
+            width: "300px",
+            height: "+=75px",   /*Relative value*/
+            marginLeft: "400px",
+            opacity: 0.5
+        }, delay);
     });
 
-    $("#up-btn").click(function(){
-        $("article").slideUp();
-    });
-    
-    // Slide down hidden paragraphs
-    $("#down-btn").click(function(){
-        $("article").slideDown();
+    $("#stop-multiple-animation").click(()=>{
+        $("#animation-multiple").stop();
+    })
+
+    $("#animation-multiple-step").click(function(){
+        $("#animation-multiple-step")
+            .animate({width: "400px"}, delay/2)
+            .animate({marginLeft: "400px"}, delay/2)
+            .animate({opacity: 0.5}, delay/2)
     });
 
-    $("article#hover > p").hover(function(){
-        $(this).addClass("destacado1");
-    }, function(){
-        $(this).removeClass("destacado1");
+    $("#stop-multiple-animation-step").click(()=>{
+//        $("#animation-multiple-step").stop(true); //it stops all animations
+//        $("#animation-multiple-step").stop(true, true); //it stops all animations but inmediately completes the stopped one
+        $("#animation-multiple-step").stop();   //it stops only the current animation. If there are chained animations, it would only stops current one
+    })
+
+    $("#animation-toggle").click(function(){
+        $("#animation-toggle").animate({
+            width: "toggle"
+        },delay, ()=>{
+            console.log("animation completed")
+        });
     });
 
-    //hover can be seen as a combination of mouseenter and mouseleave
+    $("#stop-animation-toggle").click(()=>{
+        $("#animation-toggle").stop();
+    })
+   
+    $("#caja").click(()=>{
+        $("#caja").stop().slideToggle(delay/2);     //When clicking it starts and, if it is clicked again, it stops and call slideToggle
+    });
+
+/* hover (f1,f2) --> f1=handlerin f2=handlerout
+    $("#animation-hover").hover(function(){ 
+        $(this).stop().fadeOut();
+    },function(){
+        $(this).stop().fadeIn();
+    });*/
+
+    $("#animation-hover").hover(function(){
+        $(this).stop().fadeToggle();
+    });
 
 });
-
-//show and fadeout
-$(()=>{
-    var i = 0;
-    $('input[name="keyboard"]').keypress(function(){
-        $("#keypressed").text(i += 1);
-        $("#keypressed").show().fadeOut(2000);
-    });
-});
-
-//form events
-$(()=>{
-    $('select').change(function(){
-        let ciudad=$(this).find(":selected").val();
-        alert("ha seleccionado "+ciudad)
-    });
-
-    $('input').focus(function(){
-        $(this).addClass("destacado1");
-    });
-
-    $('input').blur(function(){
-        $(this).removeClass("destacado1");
-    });
-
-    //a form can be submitted by using a submit button or by pressing enter when certain form elements have focus
-    $("form").submit(function(event){
-        event.preventDefault();
-        var regex = /^[a-zA-Z]+$/;
-        var currentValue = $('form input[name="nombre"]').val();
-
-        if(regex.test(currentValue) == false){
-            $("#texto_form").html('<p class="destacado1">Not valid!</p>').show().fadeOut(2000);
-        }
-    });
-});
-
-//Document/window events
-//ready. We have already seen since the very first sentence
-
-$(document).ready(function(){
-//    $("p").text("The DOM is now loaded and can be manipulated.");
-    console.log("The DOM is now loaded and can be manipulated.");
-});
-
-//it can be summarized by using $(()=>{ bla bla });
-
-$(()=>{
-    $(window).scroll(function() {
-        $("p#pixeles_desplazamiento").removeClass("oculto");
-        $("p#pixeles_desplazamiento").text(window.scrollY + 'px');
-    });
-});
-
-
