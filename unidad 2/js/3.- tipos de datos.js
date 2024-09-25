439abb46b9edd11e5f86d8362dffbc4d5f72a5db
avianarios <avr.web@protonmail.com>
2024-09-25 20:01:31 +0200

    Declaración de algunos números

---
+++ unidad 2/js/3.- tipos de datos.js
@@ -0,0 +1,16 @@
+//JavaScript provides a Number primitive type and a Number object
+//now we're talking about primitive type
+let numero_entero=2;
+
+//Very large numbers can be defined
+let billon=1e12;
+let millon=1e6;
+let microsegundo=1e-6;
+
+//Define octal, hexadecimal or binary numbers 
+let decimal=15;
+let binario=0b1111;
+let octal=0o17;
+let hexadecimal=0xf;
+console.log(billon);
+console.log (decimal==binario, binario==octal, octal==hexadecimal);
