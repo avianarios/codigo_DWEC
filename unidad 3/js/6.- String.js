d5c3a7ad8e274029de2e3c5e30e6c777b0570436
avianarios <avr.web@protonmail.com>
2024-10-23 21:06:10 +0200

    Más ejemplos de cadenas

--- unidad 3/js/6.- String.js
+++ unidad 3/js/6.- String.js
@@ -11,12 +11,14 @@ let lista= `Lista de compra:
     *Calabacines`;  //it has to be with inverted quotes
 let cad1="Esto es un texto aleatorio";
 let cad2="Ho\nla"; // /n is new line while /t is tab
+let texto = "JavaScript es divertido y no es difícil";
 //quotes need to be protected in order to print them inside the message
 //if they are the same to quote the message
 let cad3="SOME CHARACTERS need to be protected in order to be printed: \" \\";  
 
 //example 2: creating as an object
 let cadena_Objeto=new String("Una Cadena Objeto");
+
 
 //////////////////////////////////
 ////getting string information////
@@ -27,6 +29,30 @@ console.log (cadena_Objeto.length);
 //example 2: getting the character located at certain position
 console.log (cadena_Objeto[2], cadena_Objeto.at(2));  //two ways of accessing 3rd character
 console.log (cadena_Objeto.at(-1), cadena_Objeto[cadena_Objeto.length-1]);  //two ways of accessing the last character
+
+
+/////////////////
+////Iterating////
+/////////////////
+//Example 1: by using for...of, as strings are iterable objects
+for (let letra of cad1){
+  console.log("letra");
+}
+
+//Example 2: by converting a string into an array with split and using forEach
+cad1.split("").forEach(letra => {
+  console.log(letra);
+});
+
+//Example 3: by converting a string into an array with Array.from and using forEach
+Array.from(cad1).forEach(letra => {
+  console.log(letra); // Imprime: 'h', 'o', 'l', 'a'
+});
+
+//Example 4: by using traditional for
+for (let i = 0; i < cad1.length; i++) {
+  console.log(cad1[i]);
+}
 
 
 ////////////////////////////
@@ -50,61 +76,85 @@ console.log(cadena_Objeto);       // Una Cadena Objeto
 console.log(mayusculas, minusculas);  // UNA CADENA OBJETO, una cadena objeto
 
 //Example 3: replacing the first occurrence of a substring
-let texto="Hola Hola Hola";
+texto="Hola Hola Hola";
 console.log(texto.replace("Hola", "Adiós"));  // 'Adiós Hola Hola'
 
-//Example 4: replacing all occurrences of a substring
+//Example 4: replace can be used with regular expressions
+str = "La casa es azul y azul se va a quedar";
+console.log(str.replace(/azul/g, "verde")); // "La casa es verde y verde se va a quedar"
+
+//Example 5: replacing all occurrences of a substring
 console.log(texto.replaceAll("Hola", "Adiós")); // 'Adiós Adiós Adiós'
 
-//Example 5: removing empty spaces from the beginning and ending of a string
+//Example 6: removing empty spaces from the beginning and ending of a string
 texto="   hola   hola   hola   ";
 console.log (cadena_Objeto.trim());
 
-//Example 6: removing empty spaces only from the beginning of a string
+//Example 7: removing empty spaces only from the beginning of a string
 console.log (cadena_Objeto.trimStart());
 
-//Example 7: removing empty spaces only from the ending of a string
+//Example 8: removing empty spaces only from the ending of a string
 console.log (cadena_Objeto.trimEnd());
 
-//example 8: padStart (total_length, character) adding characters at the beginning until length is reached. If length is less than string's length, it does nothing
+//example 9: padStart (total_length, character) adding characters at the beginning until length is reached. If length is less than string's length, it does nothing
 console.log (cadena_Objeto.padStart(cadena_Objeto.length+5,"a")); //adds as many "a" at the beginning as necessary so cadena_objeto's size is cadena_Objecto.length+5
 
-//example 9: padEnd
+//example 10: padEnd
 console.log (cadena_Objeto.padEnd(cadena_Objeto.length+5, 0));
+
 
 //////////////////////////////
 ////looking for substrings////
 //////////////////////////////
-//Example 1: getting positions of a searched string 
+//Example 1: indexOf returns the position of a searched substring in a string
 let buscar="a"
 //console.log(cad3.indexOf(buscar)); 
 if (cad3.indexOf(buscar) != -1){  //returns the position, starting at position 1, where it starts the first occurrence of a aa within a string. Return -1 if not found
   console.log(`cadena ${buscar} encontrada`);
 }
-console.log(cad3.indexOf(buscar, 20)); //returns the position, starting at position 20, where it starts the first occurrence of a substring within a string
-console.log(cad3.lastIndexOf(buscar));  //returns the position where it starts the last occurrence of a substring within a string. Return -1 if not found
 
-//example 2: getting true or false if a string includes a substring
+//Example 2: the starting position from which look for the substring can be specified
+console.log(cad3.indexOf(buscar, 20)); 
+
+//Example 3: lastIndexOf returns the position where it starts the last occurrence of a substring within a string. Return -1 if not found
+console.log(cad3.lastIndexOf(buscar));
+
+//example 4: includes returns true or false depending on if a string includes a substring
 console.log (cad3.includes("ed"), cad3.includes("esto no está"));
 
-//example 3: return true if a string object starts with a substring
+//example 5: startsWith returns true if a string object starts with a substring
 console.log(cad3.startsWith("SOME"));
 
-//example 4: return true if a string object ends with a substring
+//example 6: endsWith returns true if a string object ends with a substring
 console.log (cad3.endsWith("hola"));
 
-//example 5: looking for a substring by using regular expressions
-//match belongs to String object, but it's similar to exec, which belongs to RegExp object
-let texto = "JavaScript es divertido y no es difícil";
-let resultado = texto.match(/es/);  // Busca la primera ocurrencia de "es"
-console.log(resultado);  // ["es"]
+//Example 7: search returns the position where the located text starts at the string or -1 if not found
+console.log(texto.search("JavaScript"));
 
+//Example 8: search can be used with regular expressions
+console.log(str.search(/ES/i));
 
-//example 6: looking for all occurrences by using regular expressions
-let texto = "abc123def456ghi789";
+//match can return an array with the first occurrence of the desired substring or with all occurrences if g flag is set. It returns null if not found 
+//Example 9: match usage using a string. It returns an array with the first occurrence. 
+console.log(texto.match("es"));   //"es" is a string and it's converted to /es/
+
+//Example 10: match usage with some flags. It returns an array with all occurrences 
+console.log(texto.match(/ES/gi));
+
+//Example 11: match usage with a more complex regular expression. It returns an array with all occurrences
+texto = "Las manzanas son rojas, pero los plátanos son amarillos. Mzanas voy a comprar";
+console.log(texto.match(/\b[m]\w+/gi)); // ["manzanas", "Manzanas"]
+
+//Example 12: looking for a substring by using regular expressions
+//match can be used with regular expressions
+texto = "JavaScript es divertido y no es difícil";
+console.log(texto.match(/javascript/i));
+
+//Example 13: looking for all occurrences by using regular expressions
+//matchAll can only be used with regular expressions
+texto = "abc123def456ghi789";
 let regex = /\d+/g;
-let matches = texto.matchAll(regex);
-
+let matches = texto.matchAll(regex);  //it returns an iterable with results
 for (const match of matches) {
   console.log(match[0]);  // 123, 456, 789
 }
@@ -113,23 +163,30 @@ for (const match of matches) {
 //////////////////////////
 ////getting substrings////
 //////////////////////////
-//Example 1: using substring to extract a substring. It doesn't modify original string
+//Example 1:String is an iterable object, so it works with spread operator
+let cad="hola";
+let a,b,c,d=console.log(...cad);    //returns every letter separately 
+console.log([...cad]);  //returns an array of letters
+
+//Example 2: using substring to extract a substring. It doesn't modify original string
 console.log (cad1.substring(0,4));  //extracts a substring from position 0 to 4
 console.log (cad1.substring(7));  //extracts a substring from position 7 to the end
 
-//Example 2: slice returns a substring without changing original
+//Example 3: slice returns a substring without changing original
 console.log (cad3.slice(2,10));     //returns substring from position 2 to 9
 console.log (cad3.slice(15, cad3.length-10));    //returns substring from position 15 until 11 to the end
 console.log (cad3.slice(25));  //returns substring from position 25 to the end
 console.log (cad3.slice(-10,-5));  //returns substring from 10th to the end until 5th to the end
 
-//Example 3: split returns an array of substrings separated by the argument without changing original
+//Example 4: split returns an array of substrings separated by the argument without changing original
 console.log (cad3.split(" "));
 
-//Example 4:String is an iterable object, so it works with spread operator
-let cad="hola";
-let a,b,c,d=console.log(...cad);    //returns every letter separately 
-console.log([...cad]);  //returns an array of letters
+//Example 5: split can be used with regular expressions
+let cadena = "Juan Pérez, Calle Falsa 123. María López, Avenida Siempre Viva 456; Pedro Gómez: Calle de la Amargura 789";
+let partes = cadena.split(/[,.;:\s]+/);
+console.log(partes);
+// Output: ["Juan", "Pérez", "", "Calle", "Falsa", "123", "María", "López", "", "Avenida", "Siempre", "Viva", "456", "Pedro", "Gómez", "", "Calle", "de", "la", "Amargura", "789"]
+
 
 /////////////////////////
 ////comparing strings////
@@ -163,76 +220,3 @@ console.log ("café".localeCompare("Cafe", 'es', {sensitivity: 'base'}));  //0
 var elements = [ 'casa', 'cacao', 'CAFÉ', 'café', 'california' ];
 console.log (elements.sort((a, b) => a.localeCompare(b)));
 // Returning sorted elements
-
-
-/////////////////
-////Iterating////
-/////////////////
-//Example 1: by using for...of, as strings are iterable objects
-for (let letra of cad1){
-  console.log("letra");
-}
-
-//Example 2: by converting a string into an array with split and using forEach
-cad1.split("").forEach(letra => {
-  console.log(letra);
-});
-
-//Example 3: by converting a string into an array with Array.from and using forEach
-Array.from(cad1).forEach(letra => {
-  console.log(letra); // Imprime: 'h', 'o', 'l', 'a'
-});
-
-//Example 4: by using traditional for
-for (let i = 0; i < cad1.length; i++) {
-  console.log(cad1[i]);
-}
-
-
-
-zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-
-
-//example 5: look for the first occurrence by using regular expressions
-//match returns an array with all coincidences or null if none
-let texto = "JavaScript es divertido y no es difícil";
-let resultado = texto.match(/es/);  // Busca la primera ocurrencia de "es"
-console.log(resultado);  // ["es"]
-
-
-
-//example 6: search by using modifiers
-console.log(texto.match(/es/g));  // ["es es"]  // look for any occurrence of "es"
-console.log(texto.match(/ES/i));  // ["es"]   Search for "es" string
-
-//Example: 7: search by using common characters
-const cadena = "Mi número es 12345";
-console.log(cadena.match(/\d+/)); // ["12345"]  //search in a greedy way, any number
-console.log(cadena.match(/\d+?/)); // ["1"]  //search in a non-greedy way, any number (similar as /\d/)
-console.log(cadena.match(/\bmi\b/i)); // ["Mi"] Search for "mi" word case insensitive \b represents a word boundary
-console.log(cadena.match(/\w/));
-
-
-/*
-zzzzzzzzzzzzz
-String.match(regex): Busca coincidencias en la cadena.
-String.replace(regex, replacement): Reemplaza coincidencias en la cadena.
-String.search(regex): Devuelve el índice de la primera coincidencia.
-String.split(regex): Divide la cadena en un array usando el patrón como delimitador.
-*/
-
-//Example 9: using word boundary and global modifier
-const texto = "Las manzanas son rojas, pero los plátanos son amarillos.";
-const resultado = texto.match(/\b[m]\w+/g); // ["manzanas"]
-
-//Example 10: using greedy and non-greedy pattern
-let texto = "a123b456b";
-let resultadoCodicioso=texto.match(/a.*b/);
-let resultadoNoCodicioso=texto.match(/a.*?b/);
-console.log(resultadoCodicioso, resultadoNoCodicioso);  // ["a123b456b"] ["a123b"]
-
-
-//Example 8: check if a string is an email
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const email = "ejemplo@dominio.com";
-console.log(emailRegex.exec(email)); // true
