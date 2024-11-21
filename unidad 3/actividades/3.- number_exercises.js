f91aea3dcb0bcdda0d1d8aa18c09d5ddb3425cb3
avianarios <avr.web@protonmail.com>
2024-11-21 09:58:21 +0100

    Nuevos ejercicios

---
+++ unidad 3/actividades/3.- number_exercises.js
@@ -0,0 +1,54 @@
+//1.-JavaScript Precision
+
+/* Exercise 1.- In JavaScript, 0.1 + 0.2 does not result in exactly 0.3 due to floating-point precision issues.
+        a) Write a program that adds 0.1 + 0.2 and compares the result with 0.3.
+        b) Round the result to two decimal places to verify if it matches 0.3.*/
+
+//2.-Creating a Number Object
+
+/* Exercise 2.- Create a Number object from the following values:
+    - An integer.
+    - A decimal number.
+    - A string containing a valid number.
+     Check and explain the type of each resulting object.*/
+
+//Exercise 3.- Use the Number constructor to attempt creating an object with invalid values, such as "Hello", undefined, and null.
+
+//3.-Getting the Value of a Number Object
+
+// Exercise 4.- Create a Number object with the value 42. Use the appropriate method to get its primitive value and verify its type.
+
+//4.- Checking if a Number is Finite
+/* Exercise 5.- Write a function that takes a number as a parameter and checks if it is finite or infinite. Test the function with the following values:
+    - 42
+    - Infinity
+    - -Infinity
+    - NaN*/
+
+
+
+//5.- Checking for NaN
+/* Exercise 6.- Create a function that determines if a value is NaN. Test it with:
+    - The result of 0 / 0.
+    - The result of 42 / "Hello".
+    - A valid number.*/
+
+
+//6.- Checking if a Number is an Integer
+/* Exercise 7.- Write a function that takes a number and returns true if it is an integer or false otherwise. Test it with the following values:
+    - 3
+    - 3.14
+    - -42
+    - NaN*/
+
+// 7. Conversion to String
+/* Exercise 8.- Convert the following numbers to strings using different methods and analyze the results:
+    - 42
+    - 3.14159
+    - Infinity
+    - NaN*/
+
+
+// Exercise 8.- Given the number 1234567º.89, convert it into a string formatted with thousands separators. For example: "1,234,567.89".
+
+
