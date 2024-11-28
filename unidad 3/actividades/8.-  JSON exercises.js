c953039b7b5a309b87083938ac96370c63013d56
avianarios <avr.web@protonmail.com>
2024-11-28 13:16:30 +0100

    Ejercicios del objeto JSON

---
+++ unidad 3/actividades/8.-  JSON exercises.js
@@ -0,0 +1,18 @@
+/*Exercise 1: Converting an object to JSON
+Create an object person with properties name, age, and profession. Then, convert that object to a JSON string using JSON.stringify().*/
+
+/*Exercise 2: Converting a JSON string to an object
+You have a JSON string representing an object with properties title and author. Convert that JSON string to a JavaScript object using JSON.parse(). Then, print the title and author to the console.*/
+
+/*Exercise 3: Filtering properties when converting to JSON
+Create an object with several properties, including a sensitive property called password. Use JSON.stringify() with a replacer to exclude the password property from the resulting JSON string.*/
+
+/*Exercise 4: Transform values during conversion to JSON
+Create a product object with properties name, price, and discount. Use JSON.stringify() with a replacement function to transform the values of price and discount, e.g., multiply price by 1.1 (to include VAT) and add a fixed amount to discount.*/
+
+/*Exercise 5: Retrieving a value from a deserialised object
+ You have a JSON string representing a list of students. Convert the JSON string into a JavaScript object and then access the name of the first student in the list.*/
+
+ /*Exercise 6: Using JSON.parse() with a replacement
+Convert the following JSON string to an object, but change the age value to 35:
+const json = '{"nombre": "Juan", "edad": 30, "direccion": "Madrid"}';*/
