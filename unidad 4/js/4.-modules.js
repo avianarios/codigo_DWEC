ee1b12c13651709cb3396ab38603c8e9ab5e3baa
avianarios <avr.web@protonmail.com>
2024-12-19 14:42:59 +0100

    Más explicaciones y carga dinámica

--- unidad 4/js/4.-modules.js
+++ unidad 4/js/4.-modules.js
@@ -8,10 +8,32 @@ Advantages of using modules
     -Organisation: Divide the code into smaller, more specialised files.
     -Encapsulation: Allows you to control which parts of the module are accessible from the outside and which remain private.
     -Avoid naming conflicts: Each module has its own scope.
+    -Performance: Modules can be loaded when they are needed, reducing the initial load time of the application.
+    -Bundlers, like Webpack or Rollup, are designed to work with ES6 modules. They bundle all the modules into a single file, reducing the number of requests to the server while keeping the advantages of modularity for the programmer.
+    
 
 Working with node.js needs a {  "type": "module"  } in package.json (preferred) or renaming exporting and importing files to .mjs
+
+a program should be divided into modules following the principles of modularity and separation of concerns
 */
 
+//two ways of importing and exporting code:
+//Option 1: loading several js files in the same html file and using global variables
+    //not recommended: lacks of encapsulation and possible conflict between variables. loading order is important
+<!-- index.html -->
+<script src="file1.js"></script>
+<script src="file2.js"></script>
+<script src="main.js"></script>
+
+// file1.js
+function greet() {
+    console.log("Hello, World!");
+}
+  
+// file2.js
+greet(); // Llama a la función definida en file1.js
+
+//Option 2: using modules. Recommended
 import { sumar as adding, restar as substracting } from './4.-exported-functions1.js';  //importing named functions
 import multiplicar from './4.-exported-functions1.js';    //importing the default function
 import * as operaciones from './4.-exported-functions2.js';    //importing all named functions
@@ -24,3 +46,13 @@ console.log("hola");
 console.log (operaciones.division(10, 2));
 console.log (operaciones.potencia(2, 3));
 console.log (operaciones.modulo(15, 3));
+
+
+//modules can be imported statically or dynamically
+//Dynamic import allows you to import modules at runtime, when you need them. This is called lazy loading (carga diferida o perezosa) and reduces the initial load time of the application.
+async function loadModule() {
+    const module = await import('./module.js');
+    module.myFunction(); // Usar la función importada dinámicamente
+}
+
+loadModule();
