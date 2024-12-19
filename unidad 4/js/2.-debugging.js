b232c422a76106a920ca9dc54a6317e7f838bec3
avianarios <avr.web@protonmail.com>
2024-12-19 14:42:27 +0100

    Más explicaciones

--- unidad 4/js/2.-debugging.js
+++ unidad 4/js/2.-debugging.js
@@ -1,3 +1,28 @@
+/*
+Debugging is the process of finding and fixing errors in the code. It's an essential skill for developers.
+Sending variable values to console.log is a really basic way to debug code. However, it's not the most efficient way to do it. In larger projects, it's better to use a debugger.
+
+Debugging can be done by setting breakpoints in the code. A breakpoint is a point where the execution of the code stops and the developer can inspect the values of the variables and the state of the program
+
+How to debug? 
+    -in the browser: by including the js file in an HTML file, opening it in a web server and using the browser's developer tools
+    -in VS Code. First time you run the debugger, it will create a launch.json file in the .vscode folder. This file contains the configuration of the debugger. You can modify this file to change the configuration of the debugger. VS Code will ask you to select the environment you want to debug:
+        -...node.js, that has to be installed
+        -...the browser debugger by installing an extension that connects to the browser.
+            -Install the extension ("Debugger for Firefox" in Firefox)
+            -Enable remote debugging in Firefox: about:config -> devtools.debugger.remote-enabled = true
+            -Configure remote debugger port in firefox: about:config -> devtools.debugger.remote-port = 3000 (number must be match launch.json)
+            -Configure launch.json, that it's a file created the first time you run the debugger. It's located in .vscode folder
+
+            If several configurations are present in launch.json, you can select the one you want to use by clicking on the gear icon in the debug panel and selecting the configuration you want to use.
+
+            Thus, customized configurations can be created with specific properties to adapt it to the needs of your project, such as the file you want to debug, the URL of your local server, or additional debugging options.
+
+Output is located at "Consola de depuración"
+
+*/
+
+
 // Función que suma dos números
 function suma(a, b) {
     return a + b;
