6226e62fe5e3b934999367a1b9d944baa7a80d4c
avianarios <avr.web@protonmail.com>
2025-01-08 09:57:19 +0100

    Código más adecuado para depurar e instrucciones detalladas para hacerlo

--- unidad 4/2.-debugging/2.-debugging.js
+++ unidad 4/2.-debugging/2.-debugging.js
@@ -24,33 +24,31 @@ Output is located at "Consola de depuración"
 /////////////////////////////////
 ////Example to test debugging////
 /////////////////////////////////
-//place a breakpoint at the "if" sentence of factorial function
-// watch how n changes its value at section "variables"
-// add the expression n==0 to inspect and look the result of this evaluation
-// Iterate by using:
-    // Continue (F5): Resumes the program execution until the next breakpoint. 
-    // Step over (F10): Executes the code line by line, but without stepping into called functions (i.e., it executes the code at the current level). 
-    // Step into (F11): Executes the code line by line, but if it encounters a function, it steps into it to debug as well. 
-    // Exit debugging (Shift+F11): Ends the debugging session and exits the current process, stopping the program execution.
+/*Iterate by using:
+-F5 (Continue): Resumes the program execution until the next breakpoint.
+-F10 (Step Over): Executes the code line by line, but if it encounters a function, it will execute the function completely without entering it. 
+-F11 (Step Into): Executes the code line by line, but if it encounters a function, it steps into it. 
+-Shift+F11 (Exit debugging): Ends the debugging session and exits the current process, stopping the program execution.*/
 
-// Función que suma dos números
-function suma(a, b) {
-    return a + b;
-}
+//Example 1. 
+// Place a breakpoint at "let resultadoSuma=suma(i, i+1);"" line.
+// Place a breakpoint at "let resultadoResta=resta(i,i+1);" line. Right click on the breakpoint and select "Add conditional breakpoint". Write "resultadoMultiplicacion==6" and press enter. This conditional breakpoint will stop the execution when resultadoMultiplicacion is equal to 6 and must be set after resultadoMultiplicacion is defined and assigned. It can't be done at the same line
+// Add i, suma(i,i+1), resultadoMultiplicacion and resultadoDivision to inspection to watch their values
+// Open debugging console in the bottom panel
+// Start debugging by pressing play or F5. It will stop at the first breakpoint.
+// Press F5 three times. It will stop at the next breakpoint. Watch how it stops at conditional breakpoint only at the second press (when condition is met)
+// Press F10 several times. Debugger will move to the next sentence and execute it. If it is a function, it will not step into it. Watch how variables change
+// Press F11 several times. Debugger will move to the nexte sentence and execute it. If it is a funciont, it will step into and execute it line by line. Watch how variables change
 
-// Función que calcula el factorial de un número
-function factorial(n) {
-    if (n == 0) return 1; // Caso base
-    return n * factorial(n - 1); // Llamada recursiva
-}
+let suma = (a, b) => a+b;
+let multiplicacion = (a, b) => a*b;
+let division = (a, b) => a/b;
+let resta = (a, b) => a-b;
 
-// Llamadas a las funciones
-console.log("Suma de 5 y 3:", suma(5, 3));
-
-const numero = 5;
-console.log(`Factorial de ${numero}:`, factorial(numero));
-
-// Bucle para probar puntos de interrupción
-for (let i = 1; i <= 5; i++) {
-    console.log(`Número: ${i}`);
+for (let i = 1; i <= 100; i++) {
+    let resultadoSuma=suma(i, i + 1);
+    let resultadoDivision=division(i,i+1);
+    let resultadoMultiplicacion=multiplicacion(i,i+1);
+    let resultadoResta=resta(i,i+1);
+    console.log(`suma de ${i} y ${i + 1}: ${resultadoSuma}`);
 }
