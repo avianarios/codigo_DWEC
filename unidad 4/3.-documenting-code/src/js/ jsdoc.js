9f53a8e763f1cdcaf0d102b4309171f75152ba75
alejandro <avr.web@protonmail.com>
2025-01-09 18:51:53 +0100

    Fallos reparados (era las mayúsuclas en JSdoc y los espacios)

--- unidad 4/3.-documenting-code/src/js/ jsdoc.js
+++ unidad 4/3.-documenting-code/src/js/ jsdoc.js
@@ -33,17 +33,6 @@ some jsdoc interesting parameters:
     -plugins (-p or --plugins): jsdoc ficheros_js -p plugin-name
         All of them need to be installed with npm i --save-dev plugin-name
         Some interesting plugins:
-            -markdown. markdwon is a lightweight markup language with plain text formatting syntax. It is used to write documentation in a more readable way. It is recommended to use markdown plugin to generate documentation in markdown format. Some examples:
-                # Encabezado nivel 1
-                ## Encabezado nivel 2
-                ### Encabezado nivel 3
-                **texto en negrita** ó __texto en negrita__
-                *texto en cursiva* ó _texto en cursiva_
-                Listas no ordenadas: Usar -, * o +.
-                Listas ordenadas: Usar números seguidos de un punto.
-                Enlaces: [Texto del enlace](https://ejemplo.com)
-                Imágenes: ![Texto alternativo](https://ejemplo.com/imagen.png)
-                code blocks: Usar tres acentos graves (```) antes y después del bloque de código. Se puede especificar el lenguaje de programación después de los tres acentos graves iniciales para resaltar la sintaxis.
             -markdown-plugin. It converts jsdoc comments to markdown format instead of HTML
             -jsdoc-babel. It allows you to use babel to transpile code and yet keep jsdoc working 
             -jsdoc-export-default. It improves the documentation of functions, classes, or objects exported as default in JavaScript modules. Without this plugin, JSDoc sometimes fails to properly process default exports
@@ -57,9 +46,21 @@ some jsdoc interesting parameters:
                         "jsdoc/check-param-names": "warn"
                     }
                 }
-            
-//cleverlinks: Creates automatic links within the same documentation, meaning that if you mention the name of a function, class, data type, or any other symbol that is documented within the same generated documentation, JSDoc will automatically create a link to that reference
-//monosPaceLinks: makes internal links to documentation code look like code, using a monospace typeface)
+                
+        some interesting plugins that already comes with jsdoc:
+            -markdown. markdown is a lightweight markup language with plain text formatting syntax. It is used to write documentation in a more readable way. It is recommended to use markdown plugin to generate documentation in markdown format. Some examples:
+                # Encabezado nivel 1
+                ## Encabezado nivel 2
+                ### Encabezado nivel 3
+                **texto en negrita** ó __texto en negrita__
+                *texto en cursiva* ó _texto en cursiva_
+                Listas no ordenadas: Usar -, * o +.
+                Listas ordenadas: Usar números seguidos de un punto.
+                Enlaces: [Texto del enlace](https://ejemplo.com)
+                Imágenes: ![Texto alternativo](https://ejemplo.com/imagen.png)
+                code blocks: Usar tres acentos graves (```) antes y después del bloque de código. Se puede especificar el lenguaje de programación después de los tres acentos graves iniciales para resaltar la sintaxis.
+            -cleverlinks: Creates automatic links within the same documentation, meaning that if you mention the name of a function, class, data type, or any other symbol that is documented within the same generated documentation, JSDoc will automatically create a link to that reference
+            -monosPaceLinks: makes internal links to documentation code look like code, using a monospace typeface
 */
 
 
@@ -184,10 +185,13 @@ function sumaVieja(a, b) {
 /**
  * Calcula la suma de dos números.
  *
- * **Ejemplo de uso**:
+ * ### Ejemplo de uso:
  * ```javascript
  * const resultado = sumar(2, 3); // Devuelve 5
  * ```
+ *  * ### Retorno
+ * Devuelve la suma de dos números.
+ *
  * @param {number} a - Primer número.
  * @param {number} b - Segundo número.
  * @returns {number} La suma de `a` y `b`.
@@ -225,13 +229,26 @@ function filtrarImpares(numeros) {
 
 //Example 14: @default describes the default value of a parameter
 /**
- * Saluda a una persona.
- * @param {string} [nombre="amigo"] - El nombre de la persona. Por defecto "amigo".
- * @returns {string} El saludo.
+ * Clase que representa un producto en un catálogo.
  */
-function saludar(nombre = "amigo") {
-    return `Hola, ${nombre}!`;
+class Producto {
+    /**
+     * El precio del producto.
+     * @type {number}
+     * @default 20
+     */
+    precio = 20;
+  
+    constructor(precio) {
+      if (precio) this.precio = precio;
+    }
 }
+  
+const producto1 = new Producto();
+console.log(producto1.precio);  // 20
+
+const producto2 = new Producto(30);
+console.log(producto2.precio);  // 30
 
 //Example 15: @module describes the current file as being a module. It has to be placed at the top of the file
 /**
@@ -413,3 +430,20 @@ function subtract(a, b) {
 // Uso del "espacio de nombres"
 MathLibrary.sum(5, 3);  // 8
 MathLibrary.subtract(5, 3);  // 2
+
+
+//Example: optional parameter
+/**
+ * Saluda a una persona.
+ * @param {string} [nombre="amigo"] - El nombre de la persona. Si no se proporciona, se usa "amigo".
+ * @returns {string} El saludo.
+ */
+function saludar(nombre = "amigo") {
+    return `Hola, ${nombre}!`;
+}
+
+console.log(saludar());         // "Hola, amigo!"
+console.log(saludar("Carlos")); // "Hola, Carlos!"
+
+
+//Example: markdown
