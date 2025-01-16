a22e6225d083a3d140da53156fb9a823339d0064
avianarios <avr.web@protonmail.com>
2025-01-16 17:03:36 +0100

    Reparación de fallos

--- unidad 5/js/3.-changing DOM properties and attributes.js
+++ unidad 5/js/3.-changing DOM properties and attributes.js
@@ -4,10 +4,10 @@
 //it gets the type of node
 /*most common:
     1->element-> etiquetas de nodo
-    2->text. texto dentro del nodo
-    3->atributo. atributos de un elemento
-    4->comentario-> comentarios de HTML que no son visibles, pero existen
-    9->document*/
+    2->atributo-> atributos de un elemento
+    3->text-> texto dentro del nodo
+    8->comentario-> comentarios de HTML que no son visibles, pero existen
+    9->document -> todos los elementos descienden del nodo document, es la raíz*/
 console.log (document.getElementById("titulo1").nodeType,
             document.body.firstChild.nodeType,  //the first child of body is a comment because there is no space between them. If there were a space, DOM would've created an empty #text node
             document.body.getElementsByTagName("section")[0].firstChild.nodeType, //the fist child of the section tag is an empty text node
