88131497e551cf3fa2e820a17f5d9f2b0e160508
avianarios <avr.web@protonmail.com>
2025-01-16 16:54:21 +0100

    Reorganización y limpieza de código y nuevos comentarios

--- unidad 5/js/2-selecting DOM elements.js
+++ unidad 5/js/2-selecting DOM elements.js
@@ -19,54 +19,69 @@ console.log(document.head); // <head>
 console.log(document.body); // <body>
 
 
+/*Los siguientes métodos devuelven los siguientes tipos de objetos:
+    -HTMLElement
+    -HTMLCollection
+        -Se actualiza dinámicamente si se hacen cambios en el DOM
+        -Se accede por índice numérico, por nombre o por id
+        -Se recorre como una lista iterable
+        -Se puede recorrer con for o con forEach si se convierte a Array (Array.from())
+    -NodeList
+        -No se actualiza si se hacen cambios en el DOM (salvo Node.childNodes)
+        -Sólo se accede por índice numérico
+        -Se pueden recorrer con forEach
 
-//getElementById//
-//returns an element whose id matches a passed string. Since the ids of elements are unique, this is the fastest way to select an element.
+NodeList or HTMLCollection are not Arrays. Main differences might be:
+    -array methods, like push, pop, slice, join, shift... can't be used
+    -HTMLCollection are dynamic, while Arrays are always static
+*/
+
+///////////////////////////////////////
+////Methods that return HTMLElement////
+///////////////////////////////////////
+
+//Example 1: getElementById
+//returns an element whose id matches a passed string.
 let aux=document.getElementById("parrafo1");
 console.log (aux);
 
-////getElementsByTagName////
+
+//////////////////////////////////////////
+////Methods that return HTMLCollection////
+//////////////////////////////////////////
+
+//Example 1: getElementsByTagName
 //returns a collection of all the elements present in the document that have the specified tag name, in the order of their appearance in the document.
 let aux2=document.getElementsByTagName("p");
 console.log (aux2[0], aux2.length);
 
-////getElementsByClassName////
-//returns an HTMLCollection of elements that match the passed class name. Bypassing the class names separated by whitespace, we can search for multiple class names.
+//Example 2: getElementsByClassName
+//returns an HTMLCollection of elements that match the passed class name
 let aux3=document.getElementsByClassName("parrafo_cuerpo");
 console.log (aux3[0], aux3.length);
 
-////getElementsByName////
-//returns a NodeList Collection of the elements that match the value of the name attribute with the passed string.
+
+
+////////////////////////////////////
+////Methods that return NodeList////
+////////////////////////////////////
+
+//Example 1:getElementsByName
+//returns a NodeList of the elements that match the value of the name attribute with the passed string. Useful at forms
 let aux4=document.getElementsByName("nombre");
 console.log (aux4[0],aux4.length);
 
-//all getElementsBy* returns a live collection, meaning that if an element is created after getElementsBy* was called, this collection is updated
-
-////querySelector////
-//returns the very first element within the document that matches the given selector. It only returns the element that matches with one of the specified CSS selectors, or a group of selectors.
+//Example 2: querySelector
+//returns the first element within the document that matches the given selector
 let seccion=document.querySelector("section");
 console.log(seccion);
-//the previous result can be used as an starting point
-let parrafo=seccion.querySelector("p");
-console.log(parrafo);
 
 /*body and head have their own shortctus making
 document.querySelector("body") is equivalent to document.body
 document.querySelector("head") is equivalent to document.head
 document.querySelector("html") is equivalent to document.documentElement*/
 
-
-////querySelectorAll////
-//returns a static NodeList of elements that matches with one or a group of CSS selectors. If no element matches, an empty NodeList is returned.
-//if an element is created after querySelectorAll was called, the static nodelist is NOT updated
-let art=document.querySelectorAll("article");
-console.log (art, art[0].textContent);
-
-
-let elements=document.querySelectorAll('ul > li:last-child');   //CSS selectors can be used
-console.log (elements.length, elements[0].innerHTML);   //although there is only 1 element, it is still an array
-
-/*a NodeList is not an Array. Main differences might be:
-    -array methods, like push, pop, slice, join, shift... can't be used
-    -NodeList can be live or static. Arrays are always static. Live collections allow to interact with their new members even after being selected
-*/
+//Example 3: querySelectorAll
+//returns a NodeList of elements that matches with one or a group of CSS selectors. 
+let elements=document.querySelectorAll('ul > li:last-child');
+console.log (elements.length, elements[0].innerHTML);   //although there is only 1 element, it is still an NodeList
