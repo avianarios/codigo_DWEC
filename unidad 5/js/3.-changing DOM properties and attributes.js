18b9b535191e164db80d36da21a9b3e30ddd7616
avianarios <avr.web@protonmail.com>
2025-01-18 18:08:47 +0100

    Nuevo código explicando las diferencias entre atributos y propiedades

--- unidad 5/js/3.-changing DOM properties and attributes.js
+++ unidad 5/js/3.-changing DOM properties and attributes.js
@@ -2,7 +2,7 @@
 ////getting node information////
 ////////////////////////////////
 // There most common types of node are:
-//     1->element-> etiquetas de nodo
+//     1->element-> etiquetas de nodo. Their value is always null
 //     2->atributo-> atributos de un elemento
 //     3->text-> texto dentro del nodo
 //     8->comentario-> comentarios de HTML que no son visibles, pero existen
@@ -13,13 +13,10 @@
 //     2.- Check nodeType, nodeName and nodeValue properties
     
 
-
-
-
 //Example 1: element nodes
 let titulo1=document.getElementById("titulo1");
-console.log (titulo1.nodeType, titulo1.nodeName, titulo1.nodeValue);    //1 H1 null
-console.log (document.body.nodeType, document.body.nodeName, document.body.nodeValue);
+console.log (titulo1.nodeType, titulo1.nodeName, titulo1.nodeValue);    //nodeValue of a type-1 node is always null
+console.log (document.body.nodeType, document.body.nodeName, document.body.nodeValue);      //nodeValue of a type-9 node is always null
 
 //Example 2: text nodes (type 3)
 console.log (titulo1.firstChild.nodeType, titulo1.firstChild.nodeName, titulo1.firstChild.nodeValue);
@@ -115,56 +112,110 @@ setTimeout(()=>{
 }, 2000);
 
 
-////////////////////////////////////
-////manipulating node attributes////
-////////////////////////////////////
-// Two types of atributes
-//     -standard. They have DOM properties with the same name, case insensitive
-//     -Non standard. They can be created. They are useful to pass custom data from HTML to JavaScrip or to mark HTML elements to do something with them in JavaScript. User doesn't get affected
+/////////////////////////////////
+////attributes and properties////
+/////////////////////////////////
+// Atributos: 
+//  -Son los valores definidos directamente en el marcado HTML de un elemento, como alt o src.
+//  -Se almacenan en el DOM tal como fueron definidos en el HTML.
+//  -No reflejan siempre el estado actual de los elementos, por lo que, si cambias una propiedad en JavaScript, no necesariamente se actualiza el atributo en el DOM, y viceversa. 
+//  -Se interacciona con ellos a través de los métodos getAttribute() y setAttribute()
+
+// Propiedades:
+//  -Son los valores que el navegador mantiene dinámicamente y que reflejan el estado actual de un elemento. 
+//  -No están necesariamente sincronizadas con los atributos HTML. 
+//  -Se definen a través de los objetos de los elementos en JavaScript y son accesibles a través del operador punto
+
+
+// Ejemplos de diferencias entre atributos y propiedades:
+//  -Checkbox (checked):
+//      -Atributo: input.getAttribute("checked") devuelve null o "checked", dependiendo de si el atributo está presente en el HTML inicial.
+//      -Propiedad: input.checked devuelve true o false, reflejando el estado actual del checkbox (puede cambiar dinámicamente).
+
+//  -Enlace (href):
+//      -Atributo: enlace.getAttribute("href") devuelve el valor original del atributo tal como está en el HTML, como "./pagina.html".
+//      -Propiedad: enlace.href devuelve la URL absoluta calculada por el navegador, como "http://ejemplo.com/pagina.html".
+
+//  -Disabled (disabled):
+//      -Atributo: boton.getAttribute("disabled") devuelve "disabled" si el atributo está presente, o null si no lo está.
+//      -Propiedad: boton.disabled devuelve true o false, indicando si el botón está actualmente deshabilitado.
+        
+
+//Example 1: 
+const elemento = document.querySelector("img");
+elemento.src = "nueva-imagen.jpg";  //está manipulando la propiedad src
+elemento.setAttribute("src","nueva-imagen.jpg");    //manipulando el atributo src
+
+
+// Algunos elementos del DOM tienen propiedades que no tienen un atributo correspondiente. Por ejemplo: input.checked (propiedad) refleja si un checkbox está actualmente marcado, pero el atributo checked solo tiene un valor en el HTML si se encuentra presente. Modificar la propiedad checked a través del operador de punto cambiará el estado de selección del checkbox, pero no afectará el atributo checked directamente. Otros ejemplos son disabled, selected,
+// Otros atributos no están enlazados a la propiedad del mismo nombre como, por ejemplo, href. enlace.href devuelve la URL absoluta mientras que enlace.getAttribute("href") devuelve el valor tal y como está en el HTML (que puede no ser una URL absoluta)
+// Por tanto: un atributo controla la configuración inicial y la propiedad el comportamiento actual. El atributo checked indica que está marcado inicialmente y la propiedad checked refleja si está marcado ahora (puede cambiar en tiempo real).
+// Se pueden crear atributos y propiedades personalizadas. Hay una convención que dice que su nombre debe comenzar con "data-"
+
+// ¿Cuál usar, el atributo (elemento.setAttribute("src", "nueva-imagen.jpg")) o la propiedad (elemento.src)?
+//  -¿Debe verse en el DOM porque alguna herramienta deba interaccionar con él? -> Atributos (setAttribute y getAttribute)
+//  -¿Es para uso interno de la lógica del programa? -> Propiedades (.)
+
+
+////////////////////////////////////////////////////
+////manipulating node attributes and properties////
+///////////////////////////////////////////////////
+// Two types of properties/atributes
+//     -standard. They exist as part of the HTML standard
+//     -Non standard. It doesn't exist a predefined property/attribute with the same name. They are useful to pass custom data from HTML to JavaScrip or to mark HTML elements to do something with them in JavaScript. User doesn't get affected
 
 // Example:
 //     <section class="container" data-attr="value"></div>
 //     -class -> standard
 //     -data-attr -> non standard
 
-// Manipulating...
-//     -standard attributes-> setAttribute or the name of the attribute preceeded by a dot
-//     -custom attributes -> only setAttribute
+// El nombre de los atributos personalizados, por convención, debe comenzar por "data-", pero como en los siguientes ejemplos se van a mezclar atributos y propiedades para demostrar su funcionamiento y las propiedades no pueden contener el guión, se van a omitir el data-
+ 
 
-
-//Example 1: checking element's attribute with attributes property and hasAttributes() method
+//Example 1: checking element's attribute with attributes property and hasAttribute(att_name) and hasAttributes method
 let imagen=document.querySelector("img");
-console.log(imagen.attributes);
 console.log("¿La imagen tiene atributos?:"+imagen.hasAttributes());
 console.log("¿La imagen tiene el atributo src?:"+imagen.hasAttribute("src"));
-console.log("¿La imagen tiene el atributo cámara?:"+imagen.hasAttribute("camara"));
 
-//Example 2: setting non-standard attributes
-//custom attributes don't become properties so they can only be accessed by using getAttribute
-document.body.autor="Alejandro Viana";  //doesn't work. Use setAttribute
+
+//Example 2: show all attributes
+console.log(imagen.attributes);
+
+
+//Example 3: working with custom properties
+//remember: they are not inserted into DOM and they are accessed by using dot (.)
+//watch DOM to see how it DOES NOT change
+document.body.autor="Alejandro Viana";  //we're setting a custom property. It won't be visible at the DOM
+console.log(document.body.autor);   //it works
+console.log(document.body.getAttribute("autor"));   //it doesn't work. Autor is a property, and getAttribute reads attributes
+
+imagen.autor="Procopio Máximo";
+console.log(imagen.autor);      //it works
+console.log(imagen.getAttribute("autor"));  //it doesn't work
+
+
+//Example 4: Working with custom attributes
+//remember: they are inserted into DOM and they are accessed by using setAttribute and getAttribute
+//watch DOM to see how it changes
 document.body.setAttribute('fecha','11/1/2024');
-imagen.autor="Procopio Máximo";     //doesn't work. Use setAttribute
-imagen.setAttribute("derechos_de_autor", "no");
+console.log(document.body.getAttribute("fecha"));  //it works
+console.log(document.body.fecha);   //it doesn't work. Fecha is a custom attribute and dot is used with properties
+
+imagen.setAttribute("data-derechos_de_autor", "no");
+console.log(imagen.data-derechos_de_autor);  //it doesn't work. derechos_de_autora is a attribute, not a property
+console.log(imagen.getAttribute("data-derechos_de_autor"));  //it works
 
 
-//Example 3: setting standard attributes
-//both setAttribute and dot, works
-imagen.loading="lazy";
-imagen.setAttribute("alt","Una imagen aleatoria")
-imagen.src="https://picsum.photos/300";
+//Example 5: working with standard attributes and properties
+//Most of standard properties are linked to the attributes with the same name and will change at the same time
+//watch DOM to see how it changes
+imagen.setAttribute("alt","Una imagen aleatoria");  //standard attribute
+console.log (imagen.getAttribute("alt"));       //standard attribute
+console.log (imagen.alt);       //standard property
 
-
-//Example 4: Getting non-standard attributes
-console.log (document.body.fecha)   //doesn't work. It is not a property, only a custom attribute. Use getAttribute
-console.log (document.body.getAttribute("fecha"));
-console.log (imagen.derechos_de_autor); //doesn't work. Use getAttribute
-console.log (imagen.getAttribute("derechos_de_autor"));
-
-
-//Example 5: Getting standard attributes
-//both getAttribute and dot, works
-console.log (imagen.src);   
-console.log (imagen.getAttribute("alt"));
+imagen.src="https://picsum.photos/300"; //standard property
+console.log (imagen.src);   //standard property
+console.log (imagen.getAttribute("src"));   //standard attribute
 
 
 //Example 6: checking and changing standard attribute
@@ -208,21 +259,51 @@ for (let atributo of imagen.attributes){
 //     (atributo)=>{ console.log (atributo); }
 // );
 
-//Example 10: removing attributes
+//Example 10: removing standard and custom attributes
 parrafo=document.querySelector("section#modificarClases>p#idPorEliminar");
 parrafo.removeAttribute("id");
 imagen.removeAttribute("autor");
 
+//Example 11: removing standard and custom properties
+//They can't be removed. What can be done is assigning null value
+parrafo.autor=null;
 
-//Example 11: getting classes information
-// class is an attribute than can store many values separated by spaces. Thus, it needs to be treated differently
-//     classList -> returns a DOMTokenList of classes (similar to an Array)
-//     classList.length -> returns the number of classes
-//     classList.value  -> returns the list of classes as a string
-//     classList.name -> returns all classes as a string
-//     classList.item(0) -> item is a method that returns the class located at position indicated as argument
-//     classList.contains("class") -> returns true if element has "class"
+////////////////////////////
+////Working with classes////
+////////////////////////////
+//  Class is an attribute that can store many values separated by spaces. Thus, it needs to be treated differently
 
+//  To work with classes
+//  -class: es un atributo que contiene las clases definidas en el HTML. Cuando trabajas con class, manipulas el atributo directamente, pero no tienes los métodos útiles que proporciona classList.
+//  -classList: is a property of an HTML element that returns an object that conatins some methdos to manipulate classes efficiently:
+//      -classList -> returns a DOMTokenList of classes (similar to an Array)
+//      -classList.length -> returns the number of classes
+//      -classList.value  -> returns the list of classes as a string
+//      -classList.name -> returns all classes as a string
+//      -classList.item(0) -> item is a method that returns the class located at position indicated as argument
+//      -classList.contains("class") -> returns true if element has "class"
+
+
+//Example 1: getting class information and assigning a new class with class as attribute
+const elemento = document.querySelector('#clases>p');
+console.log(elemento.class);  // Obtiene las clases como una cadena de texto.
+elemento.class = 'nueva-clase';  // Cambia el atributo class.
+
+
+//Example 2: adding a new class with class as attribute  
+const elemento = document.querySelector("#miElemento");
+let clasesExistentes = elemento.getAttribute("class");
+elemento.setAttribute("class", clasesExistentes + " nueva-clase");
+
+
+//Example 3: removing class with class as attribute
+const elemento = document.querySelector("#miElemento");
+let clasesExistentes = elemento.getAttribute("class");
+clasesExistentes = clasesExistentes.split(' ').filter(clase => clase !== 'clase-a-eliminar').join(' ');
+elemento.setAttribute("class", clasesExistentes);
+
+
+//Example 4: getting class information with class property
 listaParrafos=document.body.querySelectorAll("section#modificarClases>p");
 listaParrafos.forEach(parrafo=>{
     parrafo.classList.forEach(clase=>{
@@ -236,34 +317,35 @@ console.log(parrafo.classList.item(0));
 console.log (parrafo.classList.contains("una_clase"));   
 
 
-//Example 12: removing a class with classList.remove
+//Example 5: adding a class with classlist.add
+for (let parrafo of listaParrafos){
+    parrafo.classList.add("clase_nueva1","clase_nueva2");
+}
+
+
+//Example 6: removing a class with classList.remove
 listaParrafos.forEach(parrafo=>{
     parrafo.classList.remove("rojo");
 });
 
 
-//Example 13: removing the last class of the first paragraph
+//Example 7: removing the last class of the first paragraph
 parrafo=listaParrafos[0];
 parrafo.classList.remove(parrafo.classList.item(parrafo.classList.length-1)); 
 
 
-//Example 14: adding a class with classlist.add
-for (let parrafo of listaParrafos){
-    parrafo.classList.add("clase_nueva1","clase_nueva2");
-}
-
-//Example 16: setting a class with classList.className
+//Example 8: setting a class with classList.className with class as property
 //classname gets and set the class attribute
 console.log (listaParrafos[2].className);
 listaParrafos[2].className="unaClase";
 
 
-//Example 17: replacing classes with classList.replace(old, new)
+//Example 9: replacing classes with classList.replace(old, new)
 parrafo=listaParrafos[3];    //Selecting the first link
 parrafo.classList.replace("clase_nueva1", "clase_mas_nueva1");
 
 
-//Example 18: removing a class if it exists or creating if not with classList.toggle("clase")
+//Example 10: removing a class if it exists or creating if not with classList.toggle("clase")
 parrafo.classList.toggle("clase_guadiana");
 
 //////////////////////////////////////////
