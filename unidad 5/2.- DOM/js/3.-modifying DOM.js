0684d4a7a30979714b842958862452eb1ce722fe
avianarios <avr.web@protonmail.com>
2025-01-21 10:03:15 +0100

    Pequeños cambios en algunos ejemplos. Explicación más larga movida a fichero docs/3.-Modifying DOM.MD

--- unidad 5/2.- DOM/js/3.-modifying DOM.js
+++ unidad 5/2.- DOM/js/3.-modifying DOM.js
@@ -18,8 +18,8 @@ console.log (texto.nodeType, texto.nodeName, texto.nodeValue);
 
 //Example 2: cloning a node and checking if it is connected with cloneNode and isConnected
 // You can't insert the same element twice into the DOM. If you try, the element is moved so, at the end, you have it only once. The solution is cloning it.
-// cloneNode(arg) -> if arg=true, it also clones children. After cloning, connecting the node to the DOM is needed
-// isConnected -> returns true if the node is connected to the DOM.
+// node.cloneNode(cloneChildren) -> if cloneChildren=true, it also clones children. After cloning, connecting the node to the DOM is needed
+// node.isConnected -> returns true if the node is connected to the DOM.
 let seccion_clonada=seccion.cloneNode(true);
 console.log (seccion_clonada.isConnected);  //false
 
@@ -28,13 +28,9 @@ console.log (seccion_clonada.isConnected);  //false
 //createDocumentFragment
 //why? to avoid forcing web browser to renderize the DOM with each change, avoiding a loss of performance. You make all the changes at temporary DOM and then connect to the real one
 let estructura_temporal=document.createDocumentFragment();
-for (let i = 0; i < 10; i++) {
-    const nuevoNodo = document.createElement("div");
-    nuevoNodo.textContent = `Elemento ${i}`;
-    estructura_temporal.append(nuevoNodo); // Añade a la estructura temporal, no al DOM.
-}
-document.body.append(estructura_temporal); // Una sola inserción al DOM.
-
+//create several nodes
+//attach to estructura_temporal
+//attach estructura_temporal to the DOM
 
 
 ///////////////////////
@@ -65,17 +61,17 @@ document.body.appendChild(seccion_clonada);
 console.log (seccion_clonada.isConnected);  //true
 
 
-//Example 3: Creating a new node and inserting it before another one with insertBefore
+//Example 3: Creating a new list in a temporary structure and inserting it before a reference point
 //container_node.insertBefore(new_node, reference_node) -> inserts new_node in container_node before reference_node or after the last child if reference_node==null
-let nuevo_parrafo = document.createElement("p");
-texto = document.createTextNode("Texto insertado mediante insertBefore en section#insertar1 p:nth-of-type(3)");
-nuevo_parrafo.classList.add("insertado");
-nuevo_parrafo.appendChild(texto);
-
+estructura_temporal=document.createDocumentFragment();
+for (let i = 0; i < 10; i++) {
+    const nuevoElemento = document.createElement("p");
+    nuevoElement.textContent = `Elemento ${i}`;
+    estructura_temporal.appendChild(nuevoNodo); // Añade a la estructura temporal, no al DOM.
+}
 let padre = document.querySelector("section#insertar1");
 let nodo_referencia = document.querySelector("section#insertar1 p:nth-of-type(3)");
-
-padre.insertBefore(nuevo_parrafo, nodo_referencia);
+padre.insertBefore(estructura_temporal, nodo_referencia); // Una sola inserción al DOM.
 
 
 ////Element API (newer, supported by browsers starting at 2016). Several methods:
@@ -88,16 +84,14 @@ padre.insertBefore(nuevo_parrafo, nodo_referencia);
 //  -insertAdjacentText
 
 //Example 4: Insert a node before another one with before
-//reference_node.before(new_node) inserts new_node before reference_node as a brother
-//reference_node.before(string) inserts a string before reference_node as a brother. It inserts HTML tags as text
-
+//reference_node.before(new_node|string) inserts new_node|string before reference_node as a brother
 nuevo_comentario = document.createComment("Esto es un comentario insertado con before en section#insertar1 p:first-of-type");
 document.querySelector("section#insertar1 p:first-of-type").before(nuevo_comentario);
+document.querySelector("section#insertar1 p:last-of-type").before("<p>esto es un texto insertado como texto con before después de section#insertar1 p:last-of-type</p>")
 
 
 //Example 5: Insert a node after another one with after
-//reference_node.after(new_node) inserts new_node after reference_node as a brother
-//reference_node.after(string) inserts a string after reference_node as a brother. It inserts HTML tags as text
+//reference_node.after(new_node|string) inserts new_node|string after reference_node as a brother
 nuevo_nodo = document.createElement("p");
 nuevo_nodo.textContent = "Texto insertado mediante after en section#insertar1 p:nth-of-type(2)";
 nuevo_nodo.classList = "insertado";
@@ -106,32 +100,34 @@ document.querySelector("section#insertar1 p:nth-of-type(2)").after(nuevo_nodo);
 document.querySelector("section#insertar1 p:nth-of-type(4)").after("<p>esto es un texto insertado como texto con after antes de section#insertar1 p:nth-of-type(4)</p>")
 
 
-//Example 6: Insert with append and prepend
+//Example 6: Insert with append
 //container_node.append(new_node)-> adds a new node as the last child of the container
-//container_node.prepend(new_node)-> adds a new node as the first child of the container
-let punto_insercion1=document.querySelector("ul>ul:nth-of-type(1)");
-let punto_insercion2=document.querySelector("ul");
-
-//two ways of creating li element: innerHTML and createTextNode-append
+//At this example innerHTML (Unsecure when getting data from untrusted sources, but you have to write less code) will be used instead of createTextNode (more secure, but you have to write more code)
+let punto_insercion=document.querySelector("ul>ul:nth-of-type(1)");
 let elemento=document.createElement("li");
 elemento.classList.add("insertado");
 elemento.innerHTML="agria (insertado con append en ul>ul:nth-of-type(1))"
+punto_insercion.append(elemento);
 
-let elemento2=document.createElement("li");
-elemento2.classList.add("insertado");
+
+//Example 7: insert with prepend and createTextNode 
+//container_node.prepend(new_node)-> adds a new node as the first child of the container
+//At this example createTextNode (more secure, but you have to write more code) will be used instead of innerHTML (Unsecure when getting data from untrusted sources, but you have to write less code) 
+punto_insercion=document.querySelector("ul");
+elemento=document.createElement("li");
+elemento.classList.add("insertado");
 let fruta=document.createTextNode("mango (insertado con prepend en ul)");
-elemento2.append(fruta);
+elemento.append(fruta);
 
-punto_insercion1.append(elemento);
-punto_insercion2.prepend(elemento2);
+punto_insercion.prepend(elemento);
 
 
-//Example 7: Inserting nodes with insertAdjacentElement, insertAdjacentHTML, insertAdjacentText
+//Example 8: Inserting nodes with insertAdjacentElement, insertAdjacentHTML, insertAdjacentText
 // reference_node.insertAdjacentElement("where", inserted_node)-> inserts a created element
 // reference_node.insertAdjacentHTML("where", inserted_node) -> inserts an HTML code into DOM. No need to create an element. Much faster than innerHTML
 // reference_node.insertAdjacentText("where", inserted_node) -> inserts plain text. It treats HTML tags as text
 // "Where" can be one of the following: beforebegin, afterbegin, beforeend, afterend
-let punto_insercion=document.querySelector("section:nth-of-type(3)");
+ punto_insercion=document.querySelector("section:nth-of-type(3)");
 
 nodo=document.createElement("p");
 nodo.textContent="un texto insertado con insertAdjacentElement beforebegin en section:nth-of-type(3). Por tanto, se coloca antes del nodo section:nth-of-type(3)";
@@ -175,45 +171,6 @@ const fieldsetApellidos = formulario.querySelectorAll("fieldset")[1]; // Segundo
 fieldsetApellidos.after(fragmento);
 
 
-
-// Warning!!!!! Despite modern browsers protect you from XSS (cross-site scripting) and disable script execution when inserted, never insert HTML obtained from untrusted sources like database, forms or user input without cleaning it before. It could lead to security risks on unprotected or old web browsers 
-
-// Example 1: XSS injection:
-//   const codigoMaligo=prompt("dame el elemento a añadir");
-//   document.body.insertAdjacentHTML('beforeend', codigoMaligno);
-
-// what if the user writes <script>console.log("eres un pardillo")</script> and you have an unprotected or old web browser?
-// If you need to insert, use insertAdjacentText, innerText, textContent or similar or escape the code:
-
-// Example 1: cleaning a string
-// function escaparHTML(cadena_sucia) {
-//     const parrafo = document.createElement('p');
-//     p.textContent = cadena_sucia;
-//     return parrafo.innerHTML;
-// }
-
-// const comentarioPeligroso = "<script>alert('XSS')</script>";
-// const comentarioSeguro = escaparHTML(comentarioPeligroso);   //se inserta dentro de un <p>, por lo que ya no se ejecuta.
-// const punto_insercion = document.querySelector("#insertar1");
-// punto_insercion.insertAdjacentHTML("beforebegin", comentarioSeguro); 
-
-
-// Example 2: inserting a clean comment
-// const punto_insercion = document.querySelector("#insertar1");
-// const comentarioPeligroso = "<script>alert('XSS')</script>"; // Contenido malicioso
-// const textoNode = document.createTextNode(comentarioPeligroso); // Se convierte en texto plano, ya no hay peligro
-// punto_insercion.appendChild(textoNode);
-
-
-// Example 3: Inserting as clean text
-// const comentarioPeligroso = "<script>alert('XSS')</script>";
-// const punto_insercion = document.querySelector("#insertar1");
-
-// // Usando insertAdjacentText para insertar el comentario
-// punto_insercion.insertAdjacentText("beforebegin", comentarioPeligroso);   //Se inserta como texto plano, por lo que no se ejecuta
-
-
-
 ///////////////////////
 ////Replacing nodes////
 ///////////////////////
@@ -231,8 +188,9 @@ nodoPadre.replaceChild(nodoNuevo, nodoAntiguo);
 
 
 ////Element API: 
-//  element.replaceWith(newElement)-> Replaces the current node
-//  element.replaceChildren(...nodes) -> replaces all child nodes, at any level, of an element with the nodes passed as arguments.
+//  oldElement.replaceWith(newElement)-> Replaces oldElement with newElement
+//  oldElement.replaceChildren(...nodes) -> replaces all child nodes, at any level, of an oldElement with the nodes passed as arguments.
+
 //Example 2: Replace a node with replaceWith
 objetivo=document.querySelector("#sustituir1>p:first-of-type");
 console.log(objetivo);
@@ -245,6 +203,7 @@ parrafo.textContent="Párrafo que ha sustituido al que había aquí antes con re
 
 objetivo.replaceWith(parrafo);  
 
+//Example 3: Replace all child nodes with replaceChildren
 objetivo=document.querySelector("#sustituir2");
 parrafo=document.createElement("p");
 parrafo.textContent="Párrafo que ha sustituido al todo el bloque section que había aquí antes con replaceChildren";
@@ -269,5 +228,5 @@ borrar.isConnected ? console.log ("El elemento está conectado al DOM") : consol
 document.body.appendChild(referencia);  //the "removed" element is back!
 
 
-//Exmple 2: removing elements from the DOM with remove
-//borrar.remove();    //remove does not returns a reference to the recently removed element, so it's unrecoverable
+//Example 2: removing elements from the DOM with remove
+borrar.remove(document.querySelector(#lista_compra > ul));    //remove does not returns a reference to the recently removed element, so it's unrecoverable
