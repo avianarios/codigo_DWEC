dcba2eb0dcae8cc3e46bff4faf56784b4fa1c83a
avianarios <avr.web@protonmail.com>
2025-01-22 13:13:41 +0100

    Optimización para minimizar el código HTML

--- unidad 4/5.-polyfilling-and-transpiling-code/webpack.common.js
+++ unidad 4/5.-polyfilling-and-transpiling-code/webpack.common.js
@@ -1,15 +1,24 @@
+//Funciona, salvo el CSS
 import path from 'path';
-//import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlMinimizerPlugin from 'html-minimizer-webpack-plugin';
 
 export default {
-  entry: './fuente/index.js',
+  entry: './fuente/js/index.js',
   output: {
     path: path.resolve(process.cwd(), 'compilado', process.env.modo),
   },
   mode: process.env.modo,
-/*  plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: './fuente/index.html',
     }),
-  ],*/
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      '...',  // Esto asegura que Webpack use el minimizador de JS por defecto
+      new HtmlMinimizerPlugin(),  // Agrega el plugin para minimizar el HTML
+    ],
+  },
 };
