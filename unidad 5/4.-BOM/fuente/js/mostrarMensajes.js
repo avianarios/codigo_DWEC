556f07eb54ba2dee1ddae1e62299bfae16992689
avianarios <avr.web@protonmail.com>
2025-02-10 14:14:18 +0100

    Corrección de errores menores

--- unidad 5/4.-BOM/fuente/js/mostrarMensajes.js
+++ unidad 5/4.-BOM/fuente/js/mostrarMensajes.js
@@ -2,16 +2,16 @@
 export default function mostrarMensaje(mensaje, donde, ocultar=true) {
     const mensajeElemento = document.getElementById(donde);
     mensajeElemento.innerHTML = mensaje; // Usamos innerHTML para renderizar el contenido HTML
-    mensajeElemento.classList.add("mostrar"); // Mostrar el mensaje
+    mensajeElemento.classList.add("mostrar-msj"); // Mostrar el mensaje
   
     // Ocultar el mensaje después de 6 segundos con efecto de desvanecimiento
     if (ocultar){
         setTimeout(() => {
-            mensajeElemento.classList.add("oculto");
+            mensajeElemento.classList.add("ocultar-msj");
             setTimeout(() => {
-                mensajeElemento.classList.remove("mostrar");
+                mensajeElemento.classList.remove("mostrar-msj");
             }, 1000); // Espera a que termine la animación antes de quitar la clase
         }, 6000);
-        mensajeElemento.classList.remove("oculto");
+        mensajeElemento.classList.remove("ocultar-msj");
     }
 }
